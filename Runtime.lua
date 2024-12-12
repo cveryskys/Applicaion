bca4520b4fffe22058b1304fe21038201e5c6351
ayden <150983776+cveryskys@users.noreply.github.com>
2024-12-11 21:36:00 -0500

    Update Runtime.lua

--- Runtime.lua
+++ Runtime.lua
@@ -1,1 +1,14 @@
+--!strict
+--littlekitti
+--loads the modules
 
+for _, value in script.Parent.Services:GetDescendants() do
+	if not value:IsA("ModuleScript") then
+		continue
+	end
+
+	task.spawn(function()
+		print("loading lol")
+		local _ = (require)(value)
+	end)
+end
